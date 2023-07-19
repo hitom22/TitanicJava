de524b355139109392c499b2e0261e64eac4843b
siuuuuuuuu <siuuuuuuuu515@gmail.com>
2023-07-19 17:49:43 +0900

    [ADD] 23/07/19 OOP 회원관리 프로그램, mvc패턴 1. run - view - controller - dao 순서로 연결된 회원관리 프로그램 2. mvc패턴 적용하여 프로그래밍 완료

---
+++ src/com/kh/day18/member/controller/MemberController_prev.java
@@ -0,0 +1,112 @@
+package com.kh.day18.member.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.kh.day18.member.model.dao.MemberDAO;
+import com.kh.day18.member.model.vo.Member;
+import com.kh.day18.member.view.MemberView;
+
+public class MemberController_prev {
+//	private List<Member> mList;
+//	private MemberView view;
+	private MemberDAO memberDao;
+	
+	public MemberController_prev() {
+//		view = new MemberView();
+//		mList = new ArrayList<Member>();
+		memberDao = new MemberDAO();
+	}
+	/**
+	 * 이름으로 인덱스 찾기
+	 * @param name String으로 입력받은 값
+	 * @return int값 없으면 -1
+	 */
+	public int findIndexByName(String name) {
+		int index = memberDao.findIndexByName(name);
+		return index;
+	}
+	/**
+	 * 이름으로 회원정보 찾기
+	 * @param name
+	 * @return
+	 */
+	public Member findOneByName(String name) {
+		return memberDao.findOneByName(name);
+	}
+	/**
+	 * 회원 전체 정보 조회
+	 * @return
+	 */
+	public List<Member> allMemberList() {
+		return memberDao.allMemberList();
+	}
+	/**
+	 * 회원 정보 등록
+	 * @param member
+	 */
+	public void addMember(Member member) {
+		memberDao.addMember(member);
+	}
+	public void modifyMember(int index, Member member) {
+		memberDao.modifyMember(index, member);
+	}
+	
+	/**
+	 * 회원 정보 삭제
+	 * @param index
+	 */
+	public void subMember(int index) {
+		memberDao.subMember(index);
+	}
+	
+	
+	
+	
+//	public void startMemberProgram() {
+//		Member mOne = null;
+//		String name = "";
+//		int index = -1;
+//		finish :
+//		while(true) {
+//			int choice = view.printMenu();
+//			switch(choice) {
+//				case 1 : 
+//					mOne = view.inputMember();
+//					addMember(mOne);
+//					break;
+//				case 2 : 
+//					name = view.inputMemberName();
+//					index = findIndexByName(name);
+//					mOne = findOneByName(name);
+//					if(index != -1) {
+//						mOne = view.modifyMember(mOne);
+//						modifyMember(index, mOne);
+//					}
+//					break;
+//				case 3 : 
+//					name = view.inputMemberName();
+//					index = findIndexByName(name);
+//					subMember(index);
+//					break;
+//				case 4 : 
+//					name = view.inputMemberName();
+//					mOne = findOneByName(name);
+//					view.printOneMember(mOne);
+//					break;
+//				case 5 :
+//					view.printAllMembers(allMemberList());
+//					break;
+//				case 6 : break finish;
+//			}
+//			// 1. 회원 정보 등록
+//			// 2. 회원 정보 수정
+//			// 3. 회원 정보 삭제
+//			// 4. 회원 정보 출력(이름)
+//			// 5. 회원 전체 정보 출력
+//			// 6. 프로그램 종료
+//			
+//		}
+//	}
+}
